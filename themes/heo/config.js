6e02aad4c2281c099b2cd1d367c8f92308feda56
Chuanlong1017 <141721668+Chuanlong1017@users.noreply.github.com>
2023-12-13 19:14:17 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,12 +9,12 @@ const CONFIG = {
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_1: '分享设计',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_3: '',
+  HEO_HERO_TITLE_4: '最新作品',
+  HEO_HERO_TITLE_5: '点击查看最新作品',
+  HEO_HERO_TITLE_LINK: '',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '作品集', url: '/tag/必看精选' },
